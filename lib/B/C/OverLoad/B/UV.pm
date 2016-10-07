f5cd2dc1cff99d720f4932c16538c57679c46fd7
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2016-10-07 14:49:52 -0600

    Cleanup B::UV and add comment for SvANY since 5.24

--- lib/B/C/OverLoad/B/UV.pm
+++ lib/B/C/OverLoad/B/UV.pm
@@ -5,7 +5,7 @@ use strict;
 use B::C::Flags ();
 
 use B::C::Config;
-use B::C::File qw/xpvuvsect svsect init/;
+use B::C::File qw/svsect init/;
 use B::C::Helpers::Symtable qw/objsym savesym/;
 use B::C::Decimal qw/u32fmt/;
 
@@ -18,42 +18,83 @@ sub save {
     my $uvuformat = $B::C::Flags::Config{uvuformat};
     $uvuformat =~ s/"//g;    #" poor editor
 
-    xpvuvsect()->comment("stash, magic, cur, len, xiv_u");
-
     my $uvx  = $sv->UVX;
     my $suff = 'U';
     $suff .= 'L' if $uvx > 2147483647;
 
-    my $i = svsect()->index + 1;
-
-    # since 5.24 we need to point the xpvuv to the head
     my $u32fmt = u32fmt();
-    svsect()->add(
+    my $i      = svsect()->add(
         sprintf(
             "NULL, $u32fmt, 0x%x, {.svu_uv=${uvx}${suff}}",
             $sv->REFCNT, $sv->FLAGS
         )
     );
 
+    $sym = savesym( $sv, sprintf( "&sv_list[%d]", $i ) );
+
+=pod
+    Since 5.24 we can access the IV/NV/UV value from either the union from the main SV body
+    or also from the SvANY of it...
+
+    As IV family do not need/have one SvANY we are going to cheat....
+    by setting a 'virtual' pointer to the SvANY to an unsignificant memory address
+    but once we try to access to the IV value of it... this will point to the
+    single location where it's store in the body of the main SV....
+
+    So two differents way to access to the same memory location.
+
+=cut
+
     #32bit  - sizeof(void*), 64bit: - 2*ptrsize
     if ( $B::C::Flags::Config{ptrsize} == 4 ) {
-        init()->add( sprintf( "sv_list[%d].sv_any = (void*)&sv_list[%d] - sizeof(void*);", $i, $i ) );
+        init()->add( sprintf( "SvANY(%s) = (void*)%s - sizeof(void*);", $sym, $sym ) );
     }
     else {
-        init()->add(
-            sprintf(
-                "sv_list[%d].sv_any = (char*)&sv_list[%d] - %d;", $i, $i,
-                2 * $B::C::Flags::Config{ptrsize}
-            )
-        );
+        init()->add( sprintf( "SvANY(%s) = (char*)%s - %d;", $sym, $sym, 2 * $B::C::Flags::Config{ptrsize} ) );
     }
 
+    # TODO: we would like to use something like this, this is breaking op/64bitint.t
+    #init()->add( sprintf( "sv_list[%d].sv_any = (void*)&sv_list[%d] - STRUCT_OFFSET(XPVUV, xuv_uv);", $i, $i ) );
+
     svsect()->debug( $fullname, $sv );
-    debug(
-        sv => "Saving IV(UV) 0x%x to xpvuv_list[%d], sv_list[%d], called from %s:%s\n",
-        $sv->UVX, xpvuvsect()->index, $i, @{ [ ( caller(1) )[3] ] }, @{ [ ( caller(0) )[2] ] }
-    );
-    savesym( $sv, sprintf( "&sv_list[%d]", $i ) );
+
+    return $sym;
 }
 
 1;
+__END__
+ from sv.h
+
+ /*
+ * Bodyless IVs and NVs!
+ *
+ * Since 5.9.2, we can avoid allocating a body for SVt_IV-type SVs.
+ * Since the larger IV-holding variants of SVs store their integer
+ * values in their respective bodies, the family of SvIV() accessor
+ * macros would  naively have to branch on the SV type to find the
+ * integer value either in the HEAD or BODY. In order to avoid this
+ * expensive branch, a clever soul has deployed a great hack:
+ * We set up the SvANY pointer such that instead of pointing to a
+ * real body, it points into the memory before the location of the
+ * head. We compute this pointer such that the location of
+ * the integer member of the hypothetical body struct happens to
+ * be the same as the location of the integer member of the bodyless
+ * SV head. This now means that the SvIV() family of accessors can
+ * always read from the (hypothetical or real) body via SvANY.
+ *
+ * Since the 5.21 dev series, we employ the same trick for NVs
+ * if the architecture can support it (NVSIZE <= IVSIZE).
+ */
+
+/* The following two macros compute the necessary offsets for the above
+ * trick and store them in SvANY for SvIV() (and friends) to use. */
+
+#ifdef PERL_CORE
+#  define SET_SVANY_FOR_BODYLESS_IV(sv) \
+       SvANY(sv) =   (XPVIV*)((char*)&(sv->sv_u.svu_iv) \
+                    - STRUCT_OFFSET(XPVIV, xiv_iv))
+
+#  define SET_SVANY_FOR_BODYLESS_NV(sv) \
+       SvANY(sv) =   (XPVNV*)((char*)&(sv->sv_u.svu_nv) \
+                    - STRUCT_OFFSET(XPVNV, xnv_u.xnv_nv))
+#endif
