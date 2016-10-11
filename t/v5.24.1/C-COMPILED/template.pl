ab42e63914ef603f0476051eafbebc651f730a4f
Todd Rinaldo <toddr@cpanel.net>
2016-10-10 19:47:37 -0500

    Move PERL_USE_UNSAFE_INC to template.pl

--- t/v5.24.1/C-COMPILED/template.pl
+++ t/v5.24.1/C-COMPILED/template.pl
@@ -17,6 +17,9 @@ die "Please use perl 5.24" unless $^V =~ qr{^v5.24};
 # Used by runperl to find perlcc
 $ENV{'PROVE_BASEDIR'} = getcwd;
 
+# Some tests need . in @INC.
+$ENV{'PERL_USE_UNSAFE_INC'} = 1;
+
 use KnownErrors qw/check_todo/;
 use TestCompile qw/compile_script/;
 
