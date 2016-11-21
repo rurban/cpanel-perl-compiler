32fe6a97bebdd3db129274bc38f708f7f78a359c
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2016-11-21 11:22:08 -0700

    add an infinite loop detection

--- lib/B/C/OP.pm
+++ lib/B/C/OP.pm
@@ -3,8 +3,26 @@ package B::C::OP;
 use strict;
 use B::C::Helpers::Symtable qw/savesym objsym/;
 
+my $last;
+my $count;
+
 sub save {
     my ( $op, @args ) = @_;
+
+    if (1) {    # infinite loop detection ( for debugging purpose)
+
+        if ( $last && $last eq $op ) {
+            ++$count;
+            if ( $count == 10_000 ) {    # make this counter high enough to pass most of the common cases
+                print STDERR sprintf( "#####\n%s - %s from %s\n", ref $op, $op, 'B::C::Save'->can('stack_flat')->() );
+                die;
+            }
+        }
+        else {
+            $last  = "$op";
+            $count = 1;
+        }
+    }
 
     # cache lookup
     {
