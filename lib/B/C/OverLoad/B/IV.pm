dc56983b2a812db9c972b0aca239bea15d73e2b7
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2016-11-21 12:51:00 -0700

    use sadd for B::IV

--- lib/B/C/OverLoad/B/IV.pm
+++ lib/B/C/OverLoad/B/IV.pm
@@ -29,8 +29,8 @@ sub do_save {
 
     svsect()->debug( $fullname, $sv );
 
-    my $i = svsect()->add( sprintf( "NULL, %lu, 0x%x, {.svu_iv=%s}", $refcnt, $svflags, $ivx ) );
-    my $sym = sprintf( "&sv_list[%d]", $i );
+    my $ix = svsect()->sadd( "NULL, %lu, 0x%x, {.svu_iv=%s}", $refcnt, $svflags, $ivx );
+    my $sym = sprintf( "&sv_list[%d]", $ix );
 
 =pod
     Since 5.24 we can access the IV/NV/UV value from either the union from the main SV body
@@ -46,7 +46,7 @@ sub do_save {
 =cut
 
     # the bc_SET_SVANY_FOR_BODYLESS_IV version just uses extra parens to be able to use a pointer [need to add patch to perl]
-    init()->add( sprintf( "bc_SET_SVANY_FOR_BODYLESS_IV(%s);", $sym ) );
+    init()->sadd( "bc_SET_SVANY_FOR_BODYLESS_IV(%s);", $sym );
 
     return $sym;
 }
