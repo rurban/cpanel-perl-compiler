0dd7c3985a02febe2e4468ab13f28128a64918aa
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2016-11-21 10:13:22 -0700

    switch PVOP to use do_save

--- lib/B/C/OverLoad.pm
+++ lib/B/C/OverLoad.pm
@@ -5,7 +5,6 @@ use B::C::OverLoad::B::LEXWARN     ();    # not using objsym or savesym
 use B::C::OverLoad::B::OBJECT      ();    # nothing to save here
 use B::C::OverLoad::B::PADLIST     ();    # uses B::AV::save logic
 use B::C::OverLoad::B::PADNAMELIST ();    # uses B::AV::save logic
-use B::C::OverLoad::B::PVOP        ();
 use B::C::OverLoad::B::REGEXP      ();
 use B::C::OverLoad::B::RV          ();
 use B::C::OverLoad::B::SPECIAL     ();
@@ -22,7 +21,7 @@ BEGIN {
 
     my @OPs = qw{AV BINOP COP CV GV IV HV IO LISTOP LOGOP LOOP
       METHOP NULL NV OP PADNAME PADOP PMOP PV PVIV PVLV PVMG
-      PVNV
+      PVNV PVOP
     };
 
     # do not use @ISA, just plug what we need
